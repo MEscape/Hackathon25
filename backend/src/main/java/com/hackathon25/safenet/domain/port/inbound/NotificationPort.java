2cf08783a82163c6c46b907b5fa3d91b819d901a
Kevin Schmitt <kevinschmitt@Kevins-MacBook-Pro.local>
2025-10-25 23:59:52 +0200

    feat(emergency-notification): add EmergencyNotification service, ports, adapter and controller (#4)

--- backend/src/main/java/com/hackathon25/safenet/domain/port/inbound/NotificationPort.java
+++ backend/src/main/java/com/hackathon25/safenet/domain/port/inbound/NotificationPort.java
@@ -1,4 +1,7 @@
 package com.hackathon25.safenet.domain.port.inbound;
 
+import java.util.UUID;
+
 public interface NotificationPort {
+    void triggerEmergencyCall(UUID userId);
 }
--- backend/src/main/java/com/hackathon25/safenet/infrastructure/adapter/web/controller/EmergencyNotificationController.java
+++ backend/src/main/java/com/hackathon25/safenet/infrastructure/adapter/web/controller/EmergencyNotificationController.java
@@ -1,4 +1,48 @@
 package com.hackathon25.safenet.infrastructure.adapter.web.controller;
 
+
+import com.hackathon25.safenet.domain.port.inbound.NotificationPort;
+import com.hackathon25.safenet.domain.port.inbound.UserMetaDataPort;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/users")
+//@RequiredArgsConstructor
 public class EmergencyNotificationController {
+
+    @Autowired
+    public EmergencyNotificationController(
+            @Qualifier("emergencyNotificationService") NotificationPort notificationPort,
+            UserMetaDataPort userMetaDataPort) {
+        this.emergencyCallUseCase = notificationPort;
+        this.userMetaDataPort = userMetaDataPort;
+    }
+    private final NotificationPort emergencyCallUseCase;
+    private final UserMetaDataPort userMetaDataPort;
+
+
+
+    @PostMapping("/{userId}/emergency")
+    public ResponseEntity<String> triggerEmergency(@PathVariable UUID userId) {
+        emergencyCallUseCase.triggerEmergencyCall(userId);
+        return ResponseEntity.ok("Emergency call triggered for user " + userId);
+    }
+
+    @PostMapping("/{userId}/expoPushToken")
+    public ResponseEntity<String> registerExpoPushToken(
+            @PathVariable UUID userId,
+            @RequestParam String token) {
+
+        userMetaDataPort.updateUserMeta(userId, Map.of("expoPushToken", token));
+        return ResponseEntity.ok("Expo push token" + token + " registered for user " + userId);
+    }
 }
