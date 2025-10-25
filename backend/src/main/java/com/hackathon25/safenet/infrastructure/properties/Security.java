6064b882905a7a1b81798e928ba60d45c622c31b
Marvin Eschenbach <marvin.eschenbach@edag.com>
2025-10-25 13:35:31 +0200

    chore(config): migrate backend properties and add application configs (#1)

---
+++ backend/src/main/java/com/hackathon25/safenet/infrastructure/properties/Security.java
@@ -0,0 +1,13 @@
+package com.hackathon25.safenet.infrastructure.properties;
+
+import lombok.Data;
+
+@Data
+public class Security {
+
+    private boolean httpsOnly = false;
+
+    private String[] corsAllowedOrigins = {
+            "http://localhost:8081"
+    };
+}
